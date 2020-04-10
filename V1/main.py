e354f927b0190a46798b7a958002aa19144813f9
Matthew Wang <matthewwang2020@u.northwestern.edu>
2020-04-10 18:16:01 -0500

    update

--- V1/main.py
+++ V1/main.py
@@ -3,8 +3,8 @@ class Gap_Trader(QCAlgorithm):
 
     def Initialize(self):
         self.setConstants()
-        self.SetStartDate(2007, 12, 1)  # Set Start Date
-        self.SetEndDate(2009, 5 , 20)
+        self.SetStartDate(2020, 2, 20)  # Set Start Date
+        self.SetEndDate(2020, 3 , 20)
         self.SetCash(50000)  # Set Strategy Cash
         self.spxl = self.AddEquity("SPXL", Resolution.Minute, Market.USA, True, 1, True).Symbol
         self.spxs = self.AddEquity("SPXS", Resolution.Minute, Market.USA, True, 1, True).Symbol
@@ -21,7 +21,7 @@ class Gap_Trader(QCAlgorithm):
         self.SubscriptionManager.AddConsolidator("SPY", self.spy_consolidator)
 
     def Consolidate_Handler(self, sender, bar):
-        #self.Debug(str(bar.EndTime - bar.Time) + " " + bar.ToString())
+        self.Log(bar.Price)
         if (self.Securities["SPXL"].Holdings.HoldStock and bar.Close < bar.Open):
             self.Liquidate();
         elif (self.Securities["SPXS"].Holdings.HoldStock and bar.Close > bar.Open):
@@ -41,54 +41,34 @@ class Gap_Trader(QCAlgorithm):
         return
 
     def OnData(self, data):
-        if data.Bars.ContainsKey("SPY"):
-            self.spy_curr = data["SPY"].Price
-        if data.Bars.ContainsKey("SPXL"):
-            self.spxl_curr = data["SPXL"].Price
-        if data.Bars.ContainsKey("SPXS"):
-            self.spxs_curr = data["SPXS"].Price
-        #self.Consolidate("SPY", timedelta(minutes=5), self.fiveMinuteConsolidator)
-        #print("SPY 5 minute bar at: {0}".format(self.fiveMinuteConsolidator.Price))
         return;
 
 
     def MarketOpen(self):
-        close = self.spy_close
-        #open = self.Securities["SPY"].Price
-        open = self.spy_curr
-        #self.Log("close price: {0}".format(close))
-        #self.Log("open price: {0}".format(open))
         # Gap Up
+        close = self.close
+        open = self.Securities["SPY"].Price
         if (close < open):
-            shares = self.find_shares(close, open, self.spxs_curr)
+            shares = self.find_shares(close, open, self.Securities["SPXS"].Price)
             self.Log("Gap Up: Buy {0} SPXS Shares".format(shares))
             self.MarketOrder("SPXS", shares)
         # Gap Down
         else:
-            shares = self.find_shares(close, open, self.spxl_curr)
+            shares = self.find_shares(close, open, self.Securities["SPXL"].Price)
             self.Log("Gap Down: Buy {0} SPXL Shares".format(shares))
             self.MarketOrder("SPXL", shares)
         return
 
     def MarketClose(self):
         # Liquidate all holdings at market close
+        self.close = self.Securities["SPY"].Price
         self.Liquidate()
-
-        # Update Market Close prices
-        self.spxl_close = self.Securities["SPXL"].Price
-        self.spxs_close = self.Securities["SPXS"].Price
-        self.spy_close = self.Securities["SPY"].Price
         return
 
     def setConstants(self):
         self.granularity = 4
         self.multiplier = 10
-        self.spxl_close = 75.83
-        self.spxs_close = 11.37
-        self.spy_close =  338.34
-        self.spxl_curr = 75.83
-        self.spxs_curr = 11.37
-        self.spy_curr =  338.34
+        self.close = 338.34
         return
 
     def OnEndOfAlgorithm(self):
